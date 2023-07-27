e0c8241ee08de4b05365e9d9766a944de489144d
GustavoDaniel <danirui230390@gmail.com>
2023-07-27 02:07:11 -0300

    se agrega seccion contacto y mejora responvise

---
+++ filtro.js
@@ -0,0 +1,387 @@
+console.log("Ready!");
+
+let discos = [
+    
+    {
+        artist: "Ciro y los Persas",
+        album:"Sueños(Un Viaje En el Tiempo)",
+        trackName:"Un hombre mas",
+        trackNumber:1,
+        year:2022,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/6fa152afdef2a8136df252185eb30e4b/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist: "Ciro y los Persas",
+        album:"Sueños(Un Viaje En el Tiempo)",
+        trackName:"Ando Ganas",
+        trackNumber:2,
+        year:2022,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/6fa152afdef2a8136df252185eb30e4b/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist: "Ciro y los Persas",
+        album:"Sueños(Un Viaje En el Tiempo)",
+        trackName:"Carrousel",
+        trackNumber:3,
+        year:2022,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/6fa152afdef2a8136df252185eb30e4b/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist: "Ciro y los Persas",
+        album:"Sueños(Un Viaje En el Tiempo)",
+        trackName:"Dientes de cordero",
+        trackNumber:4,
+        year:2022,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/6fa152afdef2a8136df252185eb30e4b/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist: "Ciro y los Persas",
+        album:"Sueños(Un Viaje En el Tiempo)",
+        trackName:"Ruidos",
+        trackNumber:5,
+        year:2022,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/6fa152afdef2a8136df252185eb30e4b/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist: "Ciro y los Persas",
+        album:"Sueños(Un Viaje En el Tiempo)",
+        trackName:"Agua",
+        trackNumber:6,
+        year:2022,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/6fa152afdef2a8136df252185eb30e4b/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist: "Ciro y los Persas",
+        album:"Sueños(Un Viaje En el Tiempo)",
+        trackName:"Pacifico",
+        trackNumber:7,
+        year:2022,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/6fa152afdef2a8136df252185eb30e4b/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist: "Ciro y los Persas",
+        album:"Sueños(Un Viaje En el Tiempo)",
+        trackName:"Cancion de Cuna",
+        trackNumber:8,
+        year:2022,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/6fa152afdef2a8136df252185eb30e4b/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist: "Ciro y los Persas",
+        album:"Sueños(Un Viaje En el Tiempo)",
+        trackName:"Tal vez",
+        trackNumber:9,
+        year:2022,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/6fa152afdef2a8136df252185eb30e4b/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist: "Ciro y los Persas",
+        album:"Sueños(Un Viaje En el Tiempo)",
+        trackName:"Antes y Despues",
+        trackNumber:10,
+        year:2022,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/6fa152afdef2a8136df252185eb30e4b/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist: "Ciro y los Persas",
+        album:"Sueños(Un Viaje En el Tiempo)",
+        trackName:"Verano del 92",
+        trackNumber:11,
+        year:2022,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/6fa152afdef2a8136df252185eb30e4b/264x264-000000-80-0-0.jpg",
+    },
+    
+    ////////////////////////////////////////
+
+    {
+        artist: "Las Pastillas del Abuelo",
+        album:"Versiones",
+        trackName:"Donde esconder tantas manos",
+        trackNumber:1,
+        year:2010,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/002295bae00ff742cb6d471dec488760/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist: "Las Pastillas del Abuelo",
+        album:"Versiones",
+        trackName:"Resulta Imposible",
+        trackNumber:2,
+        year:2010,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/002295bae00ff742cb6d471dec488760/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist: "Las Pastillas del Abuelo",
+        album:"Versiones",
+        trackName:"Cerveza",
+        trackNumber:3,
+        year:2010,
+        category:"Esto es Rock papaa!, andá seguí escuchando reggaeton",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/002295bae00ff742cb6d471dec488760/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist: "Las Pastillas del Abuelo",
+        album:"Versiones",
+        trackName:"Solo Dios: Almafuerte",
+        trackNumber:4,
+        year:2010,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/002295bae00ff742cb6d471dec488760/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist: "Las Pastillas del Abuelo",
+        album:"Versiones",
+        trackName:"Tantas Escaleras",
+        trackNumber:5,
+        year:2010,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/002295bae00ff742cb6d471dec488760/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist: "Las Pastillas del Abuelo",
+        album:"Versiones",
+        trackName:"Oportunistas",
+        trackNumber:6,
+        year:2010,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/002295bae00ff742cb6d471dec488760/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist: "Las Pastillas del Abuelo",
+        album:"Versiones",
+        trackName:"Duda",
+        trackNumber:7,
+        year:2010,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/002295bae00ff742cb6d471dec488760/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist: "Las Pastillas del Abuelo",
+        album:"Versiones",
+        trackName:"Contra viento y marea",
+        trackNumber:8,
+        year:2010,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/002295bae00ff742cb6d471dec488760/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist: "Las Pastillas del Abuelo",
+        album:"Versiones",
+        trackName:"Amar y Envejecer ",
+        trackNumber:9,
+        year:2010,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/002295bae00ff742cb6d471dec488760/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist: "Las Pastillas del Abuelo",
+        album:"Versiones",
+        trackName:"Me juego el corazon",
+        trackNumber:10,
+        year:2010,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/002295bae00ff742cb6d471dec488760/264x264-000000-80-0-0.jpg",
+    },
+    
+    /////////////////////////
+    {
+        artist:"Gustavo Cerati",
+        album:"Fuerza Natural",
+        trackName:"Fuerza Natural",
+        trackNumber:1,
+        year:2009,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/633ff890de3f772c2bb6f94c56798db7/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist:"Gustavo Cerati",
+        album:"Fuerza Natural",
+        trackName:"Deja vu",
+        trackNumber:2,
+        year:2009,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/633ff890de3f772c2bb6f94c56798db7/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist:"Gustavo Cerati",
+        album:"Fuerza Natural",
+        trackName:"Magia",
+        trackNumber:3,
+        year:2009,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/633ff890de3f772c2bb6f94c56798db7/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist:"Gustavo Cerati",
+        album:"Fuerza Natural",
+        trackName:"Amor sin rodeos",
+        trackNumber:4,
+        year:2009,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/633ff890de3f772c2bb6f94c56798db7/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist:"Gustavo Cerati",
+        album:"Fuerza Natural",
+        trackName:"Traccion a sangre",
+        trackNumber:5,
+        year:2009,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/633ff890de3f772c2bb6f94c56798db7/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist:"Gustavo Cerati",
+        album:"Fuerza Natural",
+        trackName:"Desastre",
+        trackNumber:6,
+        year:2009,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/633ff890de3f772c2bb6f94c56798db7/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist:"Gustavo Cerati",
+        album:"Fuerza Natural",
+        trackName:"Rapto",
+        trackNumber:7,
+        year:2009,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/633ff890de3f772c2bb6f94c56798db7/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist:"Gustavo Cerati",
+        album:"Fuerza Natural",
+        trackName:"Cactus",
+        trackNumber:8,
+        year:2009,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/633ff890de3f772c2bb6f94c56798db7/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist:"Gustavo Cerati",
+        album:"Fuerza Natural",
+        trackName:"Naturaleza muerta",
+        trackNumber:9,
+        year:2009,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/633ff890de3f772c2bb6f94c56798db7/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist:"Gustavo Cerati",
+        album:"Fuerza Natural",
+        trackName:"Dominó",
+        trackNumber:10,
+        year:2009,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/633ff890de3f772c2bb6f94c56798db7/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist:"Gustavo Cerati",
+        album:"Fuerza Natural",
+        trackName:"Sal",
+        trackNumber:11,
+        year:2009,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/633ff890de3f772c2bb6f94c56798db7/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist:"Gustavo Cerati",
+        album:"Fuerza Natural",
+        trackName:"Convoy",
+        trackNumber:12,
+        year:2009,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/633ff890de3f772c2bb6f94c56798db7/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist:"Gustavo Cerati",
+        album:"Fuerza Natural",
+        trackName:"He visto a Lucy",
+        trackNumber:13,
+        year:2009,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/633ff890de3f772c2bb6f94c56798db7/264x264-000000-80-0-0.jpg",
+    },
+    {
+        artist:"Gustavo Cerati",
+        album:"Fuerza Natural",
+        trackName:"Numeral",
+        trackNumber:14,
+        year:2009,
+        category:"Rock",
+        albumCover:"https://e-cdn-images.dzcdn.net/images/cover/633ff890de3f772c2bb6f94c56798db7/264x264-000000-80-0-0.jpg",
+        // ref:"./disco3.html"
+        ref:"https://www.youtube.com/"
+    },
+
+
+]
+
+
+
+//buscar temas
+function buscarTemas(event) {
+    event.preventDefault();
+    let busqueda = document.getElementById('inputBusqueda').value;
+//limpiar los resultados de la busqueda anterior
+    document.getElementById('inputBusqueda').value = '';
+
+// Variable para comprobar si se encontraron coincidencias
+    var encontrado = false;
+
+    for (let i = 0; i < discos.length; i++) {
+        let tema = discos[i];
+        //buscar coincidencia de lo que busco
+        if(tema.trackName.toLowerCase().includes(busqueda.toLowerCase()) || tema.artist.toLowerCase().includes(busqueda.toLowerCase()) ){            
+            let resultadoCancion = document.createElement('p');
+            resultadoCancion.textContent = ` ${tema.trackName}` ;
+
+            //que pasa si hay dos coincidemas como por ej: Verano del 92
+            // como redireccionar en swal un link del ob
+
+            Swal.fire({
+                // titleText: busqueda,
+                titleText: `Cancion: ${tema.trackName}`,
+                text: `${tema.artist} - ${tema.album}`,
+                    imageUrl: `${tema.albumCover}`,
+                    ImageWidth: '50px',
+
+                // html:
+                //   '<a href="./disco3.html">' +
+                //   'Ir al disco',
+                // showCloseButton: true,
+                // focusConfirm: false,
+
+                showCancelButton: true,
+            //     confirmButtonText: 'Sí',
+            //     cancelButtonText: 'No'
+            //   }).then((result) => {
+            //     if (result.isConfirmed) {
+            //       // Redireccionar a la URL almacenada en la propiedad "ref"
+            //       window.location.href = discos.ref;
+            //     }
+              });
+// Cambiar la variable a true para indicar que se encontraron coincidencias
+          encontrado = true;
+        }
+    }
+    if (!encontrado) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'No se encontraron coincidencias.',
+          })
+      }
+}
+document.getElementById('btnBuscar').addEventListener('click', buscarTemas)
